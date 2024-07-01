73bb347295372e168a5fc35bb5abb5b8c8a4b56e
Rohan Sakpal <rohan.sakpal@atriina.com>
2024-07-01 16:23:00 +0530

    First Commit

---
+++ JwtWebApiTutorial/UserDto.cs
@@ -0,0 +1,8 @@
+﻿namespace JwtWebApiTutorial
+{
+    public class UserDto
+    {
+        public string Username { get;set; } = string.Empty;
+        public string Passwrod { get; set; } = string.Empty;
+    }
+}
